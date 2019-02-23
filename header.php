da434b29622d80a1fb565b11d664150c64faa28f
Marco <marco@marco.com>
2019-02-23 13:21:28 +0100

    Haciendo cambios a header.php y style.css

--- header.php
+++ header.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title></title>
+	<title><?php bloginfo('name'); ?></title>
 	<?php wp_head();?>
 </head>
 <body <?php body_class();?>>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href=""><?php bloginfo('name'); ?></a>
+	<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+</nav>
